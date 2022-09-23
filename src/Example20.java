512612b9164f213a3eb13fc2a2ccd566cfe0750c
Victor Alonso <victor@ieselcaminas.org>
2022-09-23 18:33:30 +0200

    Space

--- src/Example20.java
+++ src/Example20.java
@@ -11,7 +11,7 @@ public class Example20 {
         if (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0)) {
             System.out.println("Leap year");
 
-        }else {
+        } else {
             System.out.println("Not leap year");
         }
         
